de16027b5158a425e728043cd34360755dbb91d2
Mustafa Can Yücel <m.can.yucel.1@gmail.com>
2025-02-11 11:02:25 +0300

    Create page.svelte

---
+++ page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import StreamPlayer from '$lib/StreamPlayer.svelte';
+</script>
+
+<div class="container mx-auto px-4 my-4">
+	<div class="flex flex-col gap-4 lg:flex-row">
+		<Camera text="Camera 1" baseUrl="https://stream1.test.example.com" />
+		<Camera text="Camera 2" baseUrl="https://stream2.test.example.com"/>
+	</div>
+</div>
